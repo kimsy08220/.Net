0f6bdd888b8d0ac3c2f2740e7fac3001f155dde3
kimsy08220 <kimsy08220@naver.com>
2021-07-18 23:42:05 +0900

    Week01

---
+++ Week01/ArrayList/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections;
+
+//이것이 왜 안되는가 알아보기, object 클래스는 최상위 클래스인데 왜 최상위 클래스로 왔을까? 숙제!!!!!!!!!!
+//int a[10];
+//a[12] = 2; // 긴급확장
+
+class CSTest
+{
+	static void Main()
+	{
+        ArrayList ar = new ArrayList(10);        // 10개의 ArrayList를 잡아놓음, but 넘어가도 상관없음
+		ar.Add(1);
+		ar.Add(2.34);
+		ar.Add("string");
+		ar.Add(new DateTime(2005, 3, 1));
+        ar.Add(666);
+
+        ar.Insert(1, 1234);
+        ar.RemoveAt(0);
+
+		foreach (object o in ar)                // ar 안에 있는 원소를 o로 빼내옴
+			Console.WriteLine(o.ToString());    // javascript
+
+		Console.WriteLine("개수 : " + ar.Count);
+		Console.WriteLine("용량 : " + ar.Capacity);
+	}
+}
---
+++ Week01/MouseClick/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MouseClick
+{
+	public partial class Form1 : Form
+	{
+		private int count = 0;
+
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			Graphics G = CreateGraphics();
+			G.DrawString("Down", Font, Brushes.Black, e.X, e.Y);
+			G.Dispose();
+		}
+
+		private void Form1_MouseClick(object sender, MouseEventArgs e)
+		{
+			Graphics G = CreateGraphics();
+			G.DrawString("Click", Font, Brushes.Black, e.X, e.Y);
+			G.Dispose();
+		}
+
+		private void Form1_MouseDoubleClick(object sender, MouseEventArgs e)
+		{
+			Graphics G = CreateGraphics();
+			G.DrawString("Double Click", Font, Brushes.Black, e.X, e.Y);
+			G.Dispose();
+		}
+
+		private void Form1_Click(object sender, EventArgs e)
+		{
+			count++;
+			Text = count.ToString();
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
