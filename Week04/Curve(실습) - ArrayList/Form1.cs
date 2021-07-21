f47ecf35962805857b95070ed3532d2f9d92f016
kimsy08220 <kimsy08220@naver.com>
2021-07-21 21:47:54 +0900

    Week04

---
+++ Week04/Curve(실습) - ArrayList/Form1.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;       // ArrayList를 사용하기 위해 선언
+
+namespace Curve
+{
+	public partial class Form1 : Form
+	{
+        private ArrayList total_lines;
+        private ArrayList ar; 
+		private int x, y;
+
+		public Form1()
+		{
+			InitializeComponent();
+            total_lines = new ArrayList();
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+                ar = new ArrayList();
+				x = e.X;
+				y = e.Y;
+                ar.Add(new Point(x, y));
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (Capture && e.Button == MouseButtons.Left)       // 왼쪽 버튼을 누른 상태로 움직일 때
+			{
+				Graphics G = CreateGraphics();
+				G.DrawLine(Pens.Black, x, y, e.X, e.Y);
+				x = e.X;
+				y = e.Y;
+                ar.Add(new Point(x, y));
+				G.Dispose();
+			}
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (ArrayList line in total_lines)
+            {
+                for (int i = 1; i < line.Count; i++)
+                    e.Graphics.DrawLine(Pens.Black, (Point)line[i - 1], (Point)line[i]); 
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            total_lines.Add(ar);     // ar 배열에 있는 정보를 total_lines에 추가
+        }
+	}
+}
---
+++ Week04/Curve(실습) - CMyData(빨간선)/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+
+namespace Curve
+{
+	public partial class Form1 : Form
+	{
+        private LinkedList<CMyData> total_lines;
+        //private ArrayList ar;
+        private CMyData data;
+        private Color iCurrentPenColor;             // 현재 Color 색
+		private int x, y;
+
+		public Form1()
+		{
+			InitializeComponent();
+            total_lines = new LinkedList<CMyData>();
+            iCurrentPenColor = Color.Red;           // Red로 초기화
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+                //ar = new ArrayList();
+				x = e.X;
+				y = e.Y;
+                data = new CMyData();
+                data.AR.Add(new Point(x, y));
+                data.Color = iCurrentPenColor;      // CMyData 타입의 Color에 현재 Color 색인 Red를 저장
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (Capture && e.Button == MouseButtons.Left)       // 왼쪽 버튼을 누른 상태로 움직일 때
+			{
+				Graphics G = CreateGraphics();
+                Pen p = new Pen(data.Color);                    // Red Pen 객체 생성
+				G.DrawLine(p, x, y, e.X, e.Y);
+				x = e.X;
+				y = e.Y;
+                data.AR.Add(new Point(x, y));
+				G.Dispose();
+			}
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (CMyData line in total_lines)
+            {
+                Pen p = new Pen(line.Color);
+                for (int i = 1; i < line.AR.Count; i++)
+                    e.Graphics.DrawLine(p, (Point)line.AR[i - 1], (Point)line.AR[i]); 
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            total_lines.AddLast(data);       // ar 배열에 있는 정보를 total_lines에 추가
+        }
+
+        class CMyData
+        {
+            private ArrayList Ar;
+
+            public CMyData()  { Ar = new ArrayList(); }
+            public Color Color { get; set; }
+            public int Width { get; set; }
+            public ArrayList AR { get { return Ar; } }
+        }
+	}
+}
