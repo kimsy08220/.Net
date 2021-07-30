10c0e84e344f83797331819649865d38b7f97400
kimsy08220 <kimsy08220@naver.com>
2021-07-30 21:43:01 +0900

    Week07

---
+++ Week07/DrawImage - 반전/Form1.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DrawImage
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			Image I = Image.FromFile("아기.jpg");
+            Bitmap B = new Bitmap(I);                   // Bitmap : pixel로 이루어짐
+
+            for (int y = 0; y < B.Height; y++)
+                for (int x = 0; x < B.Width; x++)
+                {
+                    Color pixel = B.GetPixel(x, y);     // RGB pixel을 가져옴
+                     
+                    byte r = (byte)pixel.R;             // ~(byte)pixel.R : 반전
+                    byte g = (byte)pixel.G;
+                    byte b = (byte)pixel.B;
+
+                    //if (r > 255)                      // pixel은 8bit인데 오버플로우나서 까맣게 변하는 현상을 방지
+                    //    r = 255;
+                    //if (g > 255)
+                    //    g = 255;
+                    //if (b > 255)
+                    //    b = 255;
+
+                    B.SetPixel(x, y, Color.FromArgb(r, g, b));
+
+                    //int k = (r + g + b) / 3;
+                    //B.SetPixel(x, y, Color.FromArgb(k, k, k));
+                }
+            e.Graphics.DrawImage(B, 0, 0);
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
---
+++ Week07/DrawImage/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DrawImage
+{
+	public partial class Form1 : Form
+	{
+		public Form1()
+		{
+			InitializeComponent();
+		}
+
+		private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+			Image I = Image.FromFile("아기.jpg");
+			e.Graphics.DrawImage(I, 0, 0);                  // (표시 이미지, 시작 x좌표, 시작 y좌표)
+			//e.Graphics.DrawImage(I, new Point(0, 0));     // 위 코드와 동일
+
+            //e.Graphics.DrawImage(I, new Rectangle(0, 0, 200, 169)); // (표시 이미지, (시작 x좌표, 시작 y좌표, width, height))
+            //e.Graphics.DrawImage(I, 0, 0, 200, 169);      // 위 코드와 동일
+
+            //e.Graphics.DrawImage(I, 10, 10, new Rectangle(70, 20, 300, 320), GraphicsUnit.Pixel);     // (표시 이미지, 시작 x좌표, 시작 y좌표, 원래 사진(시작 x좌표, 시작 y좌표, width, height)에서 자른 사각형)
+            //e.Graphics.DrawImage(I, new Rectangle(10,10,200,200), 70, 20, 300, 300, GraphicsUnit.Pixel);  
+
+            // 사각형을 pts(지정 좌표)로 출력
+            //Rectangle R = new Rectangle(70, 20, 300, 320);
+            //Point[] pts = { new Point(0, 0), new Point(300, 0), new Point(100, 200) };
+            //e.Graphics.DrawImage(I, pts, R, GraphicsUnit.Pixel);
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+	}
+}
