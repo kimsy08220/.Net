7d9d6b9fb965cf5204fa9de6ad3802c9573b8092
kimsy08220 <kimsy08220@naver.com>
2021-08-01 00:09:29 +0900

    Week09

---
+++ Week09/DrawImage - Smoothing/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace DrawImage
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+		{
+            Image I = Image.FromFile("lenna_noise.jpg");
+            Bitmap B = new Bitmap(I);
+
+            // 흑백으로 바꾸는 작업
+            for (int y = 0; y < B.Height; y++)
+            {
+                for (int x = 0; x < B.Width; x++)
+                {
+                    Color color = B.GetPixel(x, y);
+                    byte r = (byte)((color.R + color.G + color.B) / 3);
+                    B.SetPixel(x, y, Color.FromArgb(r, r, r));
+                }
+            }
+
+            // smoothing mask 작업 : 사진을 부드럽게(노이즈를 완화) 해주는 작업
+            // 마스크 : 마스크 크기만큼의 주변 값의 영향을 받도록 하여 마스크 중심 위치의 픽셀 값을 조정하고 이를 영상 전체에 대해 적용하도록 하는 회선 기법
+	        Bitmap Smoothing = new Bitmap(B);       
+            int[,] m = { { 1, 1, 1 }, { 1, 1, 1 }, { 1, 1, 1 } };		
+            int sum;
+
+            for (int x = 1; x < B.Width - 1; x++)
+            {
+                for (int y = 1; y < B.Height - 1; y++)
+                {
+                    sum = 0;
+                    for (int r = -1; r < 2; r++)
+                    {
+                        for (int c = -1; c < 2; c++)
+                            sum += m[r + 1, c + 1] * B.GetPixel(x + r, y + c).R; 	// 마스크 씌우는 작업
+                     }
+	                sum = sum / 9;
+                    Smoothing.SetPixel(x, y, Color.FromArgb(sum, sum, sum)); 
+                }
+            }
+            e.Graphics.DrawImage(B, 0, 0);
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Week09/MDIForm/Form1.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace MDIForm
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void 새파일NToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 Child = new Form2();
+            Child.MdiParent = this;                     // this : Form1 Window
+            Child.Show();
+        }
+
+        private void 닫기CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 Child = (Form2)this.ActiveMdiChild;   // 선택된 창이 ActiveMdiChild
+            if (Child != null)
+                Child.Close();
+        }
+
+        private void 계단식정렬CToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.Cascade);
+        }
+
+        private void 수평바둑판정렬HToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileHorizontal);
+        }
+
+        private void 수직바둑판정렬VToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.TileVertical);
+        }
+
+        private void 정렬ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            LayoutMdi(MdiLayout.ArrangeIcons);
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
