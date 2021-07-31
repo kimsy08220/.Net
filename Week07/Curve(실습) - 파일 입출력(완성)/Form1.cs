e808454b05591079d18854a573809687a96b265f
kimsy08220 <kimsy08220@naver.com>
2021-07-31 20:37:06 +0900

    Week07

---
+++ Week07/Curve(실습) - 파일 입출력(완성)/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.Collections;
+using System.IO;                                        // Stream 사용하기 위해 선언
+using System.Runtime.Serialization.Formatters.Binary;   // BinaryFormatter 사용하기 위해 선언
+
+// 똑같은 프로그램으로 저장하고 불러와야 오류가 안 남(포멧이 다르기 때문)
+
+namespace Curve
+{
+	public partial class Form1 : Form
+	{
+        private LinkedList<CMyData> total_lines;
+        private CMyData data;
+        private Color CurrentPenColor;                  // 현재 Color
+        private int iCurrentPenWidth;                   // 현재 Width
+		private int x, y;
+        bool down = false;                              // 이걸 넣음으로써 더블 클릭 예외 해결
+
+		public Form1()
+		{
+			InitializeComponent();
+            total_lines = new LinkedList<CMyData>();
+            CurrentPenColor = Color.Black;           // Black로 초기화
+            iCurrentPenWidth = 2;                    // 이걸 넣음으로써 더블 클릭 예외 해결
+		}
+
+		private void Form1_MouseDown(object sender, MouseEventArgs e)
+		{
+			if (e.Button == MouseButtons.Left)
+			{
+                down = true;                        // 이걸 넣음으로써 더블 클릭 예외 해결
+				x = e.X;
+				y = e.Y;
+                data = new CMyData();
+                data.AR.Add(new Point(x, y));
+                data.Color = CurrentPenColor;      // CMyData 타입의 Color에 현재 Color인 Red를 저장
+                data.Width = iCurrentPenWidth;
+			}
+		}
+
+		private void Form1_MouseMove(object sender, MouseEventArgs e)
+		{
+			if (Capture && e.Button == MouseButtons.Left)       // 왼쪽 버튼을 누른 상태로 움직일 때
+			{
+				Graphics G = CreateGraphics();
+                Pen p = new Pen(data.Color, data.Width);        // Red Pen 객체 생성
+				G.DrawLine(p, x, y, e.X, e.Y);
+				x = e.X;
+				y = e.Y;
+                data.AR.Add(new Point(x, y));
+				G.Dispose();
+			}
+		}
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            foreach (CMyData line in total_lines)
+            {
+                Pen p = new Pen(line.Color, line.Width);
+                for (int i = 1; i < line.AR.Count; i++)
+                    e.Graphics.DrawLine(p, (Point)line.AR[i - 1], (Point)line.AR[i]); 
+            }
+        }
+
+        private void Form1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (down)                            // 이걸 넣음으로써 더블 클릭 예외 해결
+            {
+                total_lines.AddLast(data);       // ar 배열에 있는 정보를 total_lines에 추가
+                // 대화상자에서 더블 클릭하면 텅 빈 데이터를 저장, 그러면 3개의 선을 그렸지만 정작 저장된 건 4개 때문에 예외 발생
+                down = false;                    // 이걸 넣음으로써 더블 클릭 예외 해결
+            }
+        }
+
+        [Serializable]
+        class CMyData
+        {
+            private ArrayList Ar;
+
+            public CMyData()  { Ar = new ArrayList(); }
+            public Color Color { get; set; }
+            public int Width { get; set; }
+            public ArrayList AR { get { return Ar; } }
+        }
+
+        private void 대화상자ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dlg = new Form2();
+            dlg.DialogPenColor = CurrentPenColor;           // set
+            dlg.iDialogPenWidth = iCurrentPenWidth;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                CurrentPenColor = dlg.DialogPenColor;       // get 
+                iCurrentPenWidth = dlg.iDialogPenWidth;
+            }
+            dlg.Dispose();
+        }
+
+        private void 저장하기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            saveFileDialog1.InitialDirectory = @"C:\temp";
+            saveFileDialog1.Title = "파일 저장하기";
+            saveFileDialog1.Filter = "Bin 파일|*.bin|모든 파일|*.*";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                FileStream fs = new FileStream(saveFileDialog1.FileName, FileMode.Create, FileAccess.Write);
+                BinaryFormatter bf = new BinaryFormatter();         // 도움을 받음
+                bf.Serialize(fs, total_lines);                      // 파일을 바이트로 변환하여 저장
+                fs.Close();
+            }
+        }
+
+        private void 불러오기ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            openFileDialog1.InitialDirectory = @"C:\temp";
+            openFileDialog1.Title = "파일 불러오기";
+            openFileDialog1.Filter = "Bin 파일|*.bin|모든 파일|*.*";
+
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    FileStream fs = new FileStream(openFileDialog1.FileName, FileMode.Open, FileAccess.Read);
+                    BinaryFormatter bf = new BinaryFormatter();
+                    total_lines = (LinkedList<CMyData>)bf.Deserialize(fs);      // 디버그 실행 시 더블 클릭 했을 때 선 3개를 저장했는데 total_lines에는 4개를 저장 때문에 예외 발생
+                    fs.Close();
+                    Invalidate();
+                }
+                catch (FileNotFoundException)
+                {
+                    MessageBox.Show("지정한 파일이 없습니다.");
+                }
+            }   
+        }
+
+	}
+}
---
+++ Week07/DoubleBuffer/Form1.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+// 더블 버퍼링을 하기 전, 껌벅 해결 : DoubleBuffered를 true로 하면 껌벅x
+namespace DoubleBuffer
+{
+    public partial class Form1 : Form
+    {
+        int ex = 10, ey = 100;
+        const int r = 15;
+
+        public Form1()
+        {
+            InitializeComponent();
+            timer1.Start();
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            int x, y;
+
+            // 가로, 세로 줄 긋기
+            for (x = 0; x < ClientRectangle.Right; x += 10)
+                e.Graphics.DrawLine(Pens.Black, x, 0, x, ClientRectangle.Bottom);
+            for (y = 0; y < ClientRectangle.Bottom; y += 10)
+                e.Graphics.DrawLine(Pens.Black, 0, y, ClientRectangle.Right, y);
+
+            // 원 그리기
+            e.Graphics.FillEllipse(new SolidBrush(Color.LightGreen), ex - r, ey - r, r * 2, r * 2);
+            e.Graphics.DrawEllipse(new Pen(Color.Blue, 5), ex - r, ey - r, r * 2, r * 2);
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)        // 도구상자 timer를 가져오고 속성창을 클릭
+        {
+            ex += 6;
+            if (ex > ClientRectangle.Right) 
+                ex = 0;
+            Invalidate();
+        }
+    }
+}
+
+
+// 더블 버퍼링을 하고 난 후
+//namespace DoubleBuffer
+//{
+//    public partial class Form1 : Form
+//    {
+//        int ex = 10, ey = 100;
+//        const int r = 15;
+//        Bitmap B;               // 여분 도화지 역할(선이 그어져있는 도화지)
+
+//        public Form1()
+//        {
+//            InitializeComponent();
+//            timer1.Start();
+//        }
+
+//        private void Form1_Paint(object sender, PaintEventArgs e)
+//        {
+//            if (B != null)
+//                e.Graphics.DrawImage(B, 0, 0);
+//        }
+
+//        private void timer1_Tick(object sender, EventArgs e)          // 도구상자 timer를 가져오고 속성창을 클릭
+//        {
+//            int x, y;
+
+//            if (B == null || B.Width != ClientSize.Width || B.Height != ClientSize.Height)
+//                B = new Bitmap(ClientSize.Width, ClientSize.Height);
+
+//            Graphics G = Graphics.FromImage(B);
+//            G.Clear(SystemColors.Window);           // 흔적 지우기
+
+//            ex += 6;
+//            if (ex > ClientRectangle.Right) 
+//                ex = 0;
+
+//            // 가로, 세로 줄 긋기
+//            for (x = 0; x < ClientRectangle.Right; x += 10)
+//                G.DrawLine(Pens.Black, x, 0, x, ClientRectangle.Bottom);
+//            for (y = 0; y < ClientRectangle.Bottom; y += 10)
+//                G.DrawLine(Pens.Black, 0, y, ClientRectangle.Right, y);
+
+//            // 원 그리기
+//            G.FillEllipse(new SolidBrush(Color.LightGreen), ex - r, ey - r, r * 2, r * 2);
+//            G.DrawEllipse(new Pen(Color.Blue, 5), ex - r, ey - r, r * 2, r * 2);
+
+//            Invalidate();       // Paint로 이동
+//        }
+//    }
+//}
