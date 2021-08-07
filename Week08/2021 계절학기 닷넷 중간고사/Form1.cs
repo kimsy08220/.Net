1c00da9c2b7b4a825e44ae86437c668aab11b7b2
kimsy08220 <kimsy08220@naver.com>
2021-08-08 00:03:18 +0900

    Week08

---
+++ Week08/2021 계절학기 닷넷 중간고사/Form1.cs
@@ -0,0 +1,113 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Collections;               // ArrayList 사용하기 위해 선언
+
+namespace _2021_계절학기_닷넷_중간고사
+{
+    public partial class Form1 : Form
+    {
+        private Color CurrentColor;
+        private int CurrentShape;
+        private ArrayList ar;
+        CMyData data;
+        Bitmap B;
+
+        class CMyData
+        {
+            public Point Point { get; set; }
+            public Color Color { get; set; }
+            public int Shape { get; set; }
+        }
+
+        public Form1()
+        {
+            InitializeComponent();
+            ar = new ArrayList();
+            CurrentColor = Color.Black;
+            CurrentShape = 0;
+        }
+
+        private void Form1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                data = new CMyData();
+                data.Point = new Point(e.X, e.Y);
+                data.Color = CurrentColor;
+                data.Shape = CurrentShape;
+                ar.Add(data);
+            }
+            Invalidate();
+
+            //Graphics G = CreateGraphics();
+            //Pen p = new Pen(data.Color);
+            //SolidBrush brc = new SolidBrush(data.Color);
+
+            //if (data.Shape == 0)
+            //{
+            //    G.DrawRectangle(p, data.Point.X, data.Point.Y, 10, 10);
+            //    G.FillRectangle(brc, data.Point.X, data.Point.Y, 10, 10);
+            //}
+            //else if (data.Shape == 1)
+            //{
+            //    G.DrawEllipse(p, data.Point.X, data.Point.Y, 10, 10);
+            //    G.FillEllipse(brc, data.Point.X, data.Point.Y, 10, 10);
+            //}
+        }
+
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+            if (B == null || B.Width != ClientSize.Width || B.Height != ClientSize.Height)
+                B = new Bitmap(ClientSize.Width, ClientSize.Height);
+            
+            Graphics G = Graphics.FromImage(B);
+            G.Clear(SystemColors.Window);
+
+            foreach (CMyData c in ar)
+            {
+                Pen p = new Pen(c.Color);
+                SolidBrush brc = new SolidBrush(c.Color);
+
+                if (c.Shape == 0)
+                {
+                    G.DrawRectangle(p, c.Point.X, c.Point.Y, 10, 10);
+                    G.FillRectangle(brc, c.Point.X, c.Point.Y, 10, 10);
+                }
+                else if (c.Shape == 1)
+                {
+                    G.DrawEllipse(p, c.Point.X, c.Point.Y, 10, 10);
+                    G.FillEllipse(brc, c.Point.X, c.Point.Y, 10, 10);
+                }
+            }
+
+            if (B != null)
+                e.Graphics.DrawImage(B, 0, 0);
+        }
+
+        protected override void OnPaintBackground(PaintEventArgs e)     // Bitmap
+        {
+            //base.OnPaintBackground(e);
+        }
+
+        private void 대화상자ToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Form2 dlg = new Form2();
+            dlg.DialogColor = CurrentColor;
+            dlg.DialogShape = CurrentShape;
+
+            if (dlg.ShowDialog() == DialogResult.OK)
+            {
+                CurrentColor = dlg.DialogColor;
+                CurrentShape = dlg.DialogShape;
+            }
+            dlg.Dispose();
+        }
+    }
+}
---
+++ Week08/2021 계절학기 닷넷 중간고사/Form2.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2021_계절학기_닷넷_중간고사
+{
+    public partial class Form2 : Form
+    {
+        public Color DialogColor { get; set; }
+        public int DialogShape;
+
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void Form2_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("사각형");
+            comboBox1.Items.Add("타원");
+
+            if (DialogShape == 0) 
+                comboBox1.Text = "사각형";
+            if (DialogShape == 1) 
+                comboBox1.Text = "타원";
+
+            hScrollBar1.Value = DialogColor.R;
+            hScrollBar2.Value = DialogColor.G;
+            hScrollBar3.Value = DialogColor.B;
+
+            textBox1.Text = DialogColor.R.ToString();
+            textBox2.Text = DialogColor.G.ToString();
+            textBox3.Text = DialogColor.B.ToString();
+        }
+
+        private void hScrollBar_Scroll(object sender, ScrollEventArgs e)
+        {
+            DialogColor = Color.FromArgb(hScrollBar1.Value, hScrollBar2.Value, hScrollBar3.Value);
+            textBox1.Text = hScrollBar1.Value.ToString();
+            textBox2.Text = hScrollBar2.Value.ToString();
+            textBox3.Text = hScrollBar3.Value.ToString();
+            label5.Invalidate();
+        }
+
+        private void comboBox1_TextChanged(object sender, EventArgs e)
+        {
+            DialogShape = comboBox1.SelectedIndex;
+            if (DialogShape == 0) 
+                comboBox1.Text = "사각형";
+            if (DialogShape == 1) 
+                comboBox1.Text = "타원";
+            label5.Invalidate();
+        }
+
+        private void label5_Paint(object sender, PaintEventArgs e)
+        {
+            Pen p = new Pen(DialogColor);
+            SolidBrush brc = new SolidBrush(DialogColor);
+
+            if (DialogShape == 0)
+            {
+                e.Graphics.DrawRectangle(p, label5.Width / 2 - 25, label5.Height / 2 - 25, 50, 50);
+                e.Graphics.FillRectangle(brc, label5.Width / 2 - 25, label5.Height / 2 - 25, 50, 50);
+            }
+            else if (DialogShape == 1)
+            {
+                e.Graphics.DrawEllipse(p, label5.Width / 2 - 25, label5.Height / 2 - 25, 50, 50);
+                e.Graphics.FillEllipse(brc, label5.Width / 2 - 25, label5.Height / 2 - 25, 50, 50);
+            }
+        }
+    }
+}
